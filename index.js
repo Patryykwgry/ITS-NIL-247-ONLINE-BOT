c1f31231f0a5cb8ea039e9c6cf22fb506c94aad6
Patryykwgry <misiurapatryk579@gmail.com>
2026-02-20 13:10:25 +0100

    Server 24/7

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'sgshowriyaff.aternos.me', 
-  version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
-  username: 'SG_SHOWRIYA', 
-  port: 28648, 
+  host: 'Anarchia-sv.aternos.me', 
+  version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.21.4'
+  username: 'AnarchiaSVBOT', 
+  port: 40816, 
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
